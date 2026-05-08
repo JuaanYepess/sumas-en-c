2ee1c82ee5c4b5106c12dc887302bf2dc16a2974
Juan Yepes <laboratorio@udla-ec.int>
2026-05-08 14:48:54 -0500

    agregamos resta

--- main.c
+++ main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-
-int sumar(int a, int b);
-
+#include "restar.h"
+#include "sumar.h"
 int main(int argc, char const *argv[])
 {
     int a, b;
 
-    printf("Ingrese el 1er número: ");
+    printf("Ingrese el 1er numero: ");
     scanf("%d", &a);
 
-    printf("Ingrese el 2do número: ");
+    printf("Ingrese el 2do numero: ");
     scanf("%d", &b);
 
     printf("La suma es: %d", sumar(a, b));
+ printf("La resta es: %d", sumar(a, b));
 
     return 0;
 }
 
-int sumar(int a, int b)
-{
-    return a + b;
-}
